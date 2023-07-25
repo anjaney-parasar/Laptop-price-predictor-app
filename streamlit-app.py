873cc2bdf585e718b355f652bbc3d0cdc62ddfc7
Anjaney Parasar <parasaranjaney@gmail.com>
2023-07-25 22:41:51 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st	
 import pickle
+import joblib
 from joblib import dump, load
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.preprocessing import LabelEncoder, StandardScaler	
